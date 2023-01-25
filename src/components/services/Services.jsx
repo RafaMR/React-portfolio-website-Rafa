5056834b118046c31b09fe5138f547923310cbc9
Rafa Merayo <merayo.rafa@gmail.com>
2023-01-25 19:07:14 +0100

    Component services finished

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,70 @@
 import React from 'react';
 import './services.css';
+import { BiCheck } from 'react-icons/bi';
 
 const Services = () => {
-  return <section id="services">Services</section>;
+  return (
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>What to expect from me</h2>
+      <div className="container services-container">
+        <article className="service">
+          <div className="service-head">
+            <h3>Soft Skills</h3>
+          </div>
+
+          <ul className="service-list">
+            <li>
+              <BiCheck className="service-list-icon" />
+              <p>
+                Eagerness to learn, I'm motivated to deepen my web development
+                knowledge.
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service-list-icon" />
+              <p>
+                Teamwork. I have learned during career and my time at Ironhack
+                that working with your team is of uttermost importance.
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service-list-icon" />
+              <p>
+                Perseverance, gaining a solid knowledge of any subject takes
+                time and effort.{' '}
+              </p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF SOFT SKILLS */}
+        <article className="service">
+          <div className="service-head">
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className="service-list">
+            <li>
+              <BiCheck className="service-list-icon" />
+              <p>Deep knowledege in HTML, CSS and JavaScript</p>
+            </li>
+            <li>
+              <BiCheck className="service-list-icon" />
+              <p>Backend Knowledge including NodeJS, Express.js and MongoDb</p>
+            </li>
+            <li>
+              <BiCheck className="service-list-icon" />
+              <p>React knowledge including Components and Hooks</p>
+            </li>
+            <li>
+              <BiCheck className="service-list-icon" />
+              <p>Currently learning mySQL to improve my development skills</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
